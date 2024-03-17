e560edba647c54af6f1ca2b850f91104a09b8c80
LiuYuYang01 <3311118881@qq.com>
2024-03-17 15:30:19 +0800

    完成Socket.io私聊功能

--- src/io/Chat.py
+++ src/io/Chat.py
@@ -1,5 +1,15 @@
+from flask_socketio import emit, join_room
+
 from src import socketio
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
+
+@socketio.on('msg')
+def handle_message(data):
+    emit('msg', data, broadcast=True)  # 对所有房间传播
+
+
+@socketio.on('room')
+def joinRoom(data, room):
+    join_room(room)
+
+    emit('room', data, room=room)
