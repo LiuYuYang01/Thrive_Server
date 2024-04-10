8a3c529c74986a899559b97ecc0a6ba659dee07e
Liu 宇阳 <103110550+LiuYuYang01@users.noreply.github.com>
2024-04-10 20:59:55 +0800

    Add files via upload

---
+++ src/model/system/LayoutModel.py
@@ -0,0 +1,5 @@
+class LayoutModel(object):
+        isArticleLayout = "classics"
+        rightSidebar = ['author', 'randomArticle', 'hotArticle', 'newComments']
+        swiperImage = "https://bu.dusays.com/2023/11/10/654e2cf6055b0.jpg"
+        swiperText = ['System.out.print("有些梦虽然遥不可及，但并不是不可能实现!");', 'print(" 互联网从不缺乏天才, 而努力才是最终的入场券!")', 'console.log("再渺小的星光，也有属于他的光芒!")']
