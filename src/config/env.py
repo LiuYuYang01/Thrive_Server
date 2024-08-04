f94c3cc8ea367c6a332d0140f5f4cc10592f3a3f
宇阳 <3311118881@qq.com>
2024-08-04 15:48:20 +0800

    解决MySQLdb安装不上问题

--- src/config/env.py
+++ src/config/env.py
@@ -3,12 +3,12 @@ from src.config import BaseConfig
 
 # 开发环境
 class DevelopConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:123123@127.0.0.1:3306/thrive'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:liuyuyang@127.0.0.1:3306/ThriveX'
 
 
 # 生产环境
 class ProduceConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:123123@127.0.0.1:3306/thrive'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:liuyuyang@127.0.0.1:3306/ThriveX'
 
 
 # 选择环境
